23c97288c8ed3323bb25a98cfaf27487748a0f3a
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-11-16 09:36:44 +0100

    First Snack

--- src/snacks.java
+++ src/snacks.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class snacks {
     public static void main(String[] args) {
-        
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Inserire un numero:");
+        int number = sn.nextInt();
+
+        if ((number % 2) == 0) {
+            System.out.println(number);
+        }else{
+            System.out.println((number+1));
+        }
     }
 }
