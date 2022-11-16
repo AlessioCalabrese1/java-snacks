78fedee1fbff932c9d13148b5036a371cc454ae3
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-11-16 12:01:11 +0100

    Snack 3

--- src/snacks.java
+++ src/snacks.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class snacks {
     public static void main(String[] args) {
+
+        //Snack 1
+
         // Scanner sn = new Scanner(System.in);
         // System.out.println("Inserire un numero:");
         // int number = sn.nextInt();
@@ -13,14 +16,31 @@ public class snacks {
         //     System.out.println((number+1));
         // }
 
-        String[] names = {"Papà", "Zio", "Pinco"};
-        String[] surnames = {"Catoro", "Pera", "Pallino"};
 
-        for (int i = 0; i < names.length; i++) {
-            Random rnd = new Random();
-            int rndName = rnd.nextInt(names.length);
-            int rndSurname = rnd.nextInt(surnames.length);
-            System.out.println(names[rndName] + " " + surnames[rndSurname]);
+        //Snack 2
+
+        // String[] names = {"Papà", "Zio", "Pinco"};
+        // String[] surnames = {"Catoro", "Pera", "Pallino"};
+
+        // for (int i = 0; i < names.length; i++) {
+        //     Random rnd = new Random();
+        //     int rndName = rnd.nextInt(names.length);
+        //     int rndSurname = rnd.nextInt(surnames.length);
+        //     System.out.println(names[rndName] + " " + surnames[rndSurname]);
+        // }
+
+
+        //Snack 3
+
+        int[] nmb = {1,2,3,4,5,6,7,8,9};
+        
+        int sumDis = 0; 
+        for (int i = 0; i < nmb.length; i++) {
+            if ((i % 2) == 1) {
+                sumDis += nmb[i]; 
+            }
         }
+
+        System.out.println("La somma dei numeri dispari è: " + sumDis);
     }
 }
